b0eb7767c5adf609dd076a2c7dadf1d438d5539d
Danial Ansari <danialansari998@gmail.com>
2020-09-02 10:13:31 +0500

    first commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt,BigDecimal } from "@graphprotocol/graph-ts"
 import {
   Contract,
   Approval,
@@ -6,32 +6,12 @@ import {
   OwnershipTransferred,
   Transfer
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { user } from "../generated/schema"
 
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._owner = event.params._owner
-  entity._spender = event.params._spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
+  
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
   // `new Entity(...)`, set the fields that should be updated and save the
@@ -66,6 +46,46 @@ export function handleApproval(event: Approval): void {
 
 export function handleBurn(event: Burn): void {}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
 
-export function handleTransfer(event: Transfer): void {}
+
+}
+
+let owner="0xb23d254dbd37f5fddd2a707794bf97783c821d12";
+let firstTime=true;
+export function handleTransfer(event: Transfer): void {
+  let account1=user.load(event.params._from.toHex())
+
+  if(account1==null){
+    account1=new user(event.params._from.toHex());
+    account1.balance=BigDecimal.fromString("0");
+  }
+  
+  let account2=user.load(event.params._to.toHex())
+
+  if(account2==null){
+    account2=new user(event.params._to.toHex());
+    account2.balance=BigDecimal.fromString("0");
+  }
+
+  let ownerAccount=user.load(owner);
+
+
+  account1.balance=account1.balance.minus(BigDecimal.fromString(event.params._amount.toString()))
+  account2.balance=account2.balance.plus(BigDecimal.fromString(event.params._amount.toString()))
+
+
+  if(event.transaction.hash.toHexString()=="0xff8a46ec40a02cfa1fc0b484855369ced2016e8ca897847eab6c0cf74d50c1e2"){
+    
+    account1.hash=event.transaction.hash.toHexString()
+    account1.balance=account1.balance.plus(BigDecimal.fromString("110000000000000000000000000"));
+  }
+  
+
+
+
+
+  account1.save();
+  account2.save();
+  
+}
